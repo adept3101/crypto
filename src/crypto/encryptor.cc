2fe8e7e6e3089803571052f57660f346e1cadf4d
adept <timofeysitnikov@mail.ru>
2026-09-22 21:18:28 +0300

    encrypt

--- src/crypto/encryptor.cc
+++ src/crypto/encryptor.cc
@@ -1,28 +1,46 @@
 #include "encryptor.h"
-#include <iterator>
-#include <iostream>
-#include <sodium/crypto_aead_chacha20poly1305.h>
 
-void encrypt(std::string str) {
+#include <boost/beast/core/detail/base64.hpp>
+#include <stdexcept>
+
+std::string encrypt(std::string str) {
   unsigned char key[crypto_aead_chacha20poly1305_ietf_KEYBYTES];
   unsigned char nonce[crypto_aead_chacha20poly1305_ietf_NPUBBYTES];
-  
+
   size_t size_str = std::size(str);
 
-  unsigned char
-      cipher_text[size_str + crypto_aead_chacha20poly1305_ietf_ABYTES];
+  std::string cipher_text(
+      std::size(str) + crypto_aead_chacha20poly1305_ietf_ABYTES, '\0');
 
   unsigned long long cipher_len;
   randombytes_buf(key, sizeof key);
   randombytes_buf(nonce, sizeof nonce);
 
-  int ae = crypto_aead_xchacha20poly1305_ietf_encrypt(
-      cipher_text, &cipher_len, (const unsigned char *)&str, size_str,
+  int ae = crypto_aead_chacha20poly1305_ietf_encrypt(
+      reinterpret_cast<unsigned char *>(cipher_text.data()), &cipher_len,
+      (const unsigned char *)&str, size_str,
       NULL, // additional authenticated data
       0, NULL, nonce, key);
 
-  for (int i = 0; i < cipher_len; i++) {
-    std::cout << cipher_text[i];
+  if (ae != 0) {
+    throw std::runtime_error("encrypt failed");
   }
+
+  cipher_text.resize(cipher_len);
+  return cipher_text;
 }
 
+std::string encode(const std::string &data) {
+  std::string res;
+  res.resize(base64::encoded_size(data.size()));
+
+  base64::encode(
+      res.data(),
+      reinterpret_cast<const uint8_t*>(data.data()),
+      data.size()
+      );
+
+  return res;
+  // return boost::beast::detail::base64::encode(
+  //     reinterpret_cast<const uint8_t *>(data.data()), data.size());
+}
--- src/crypto/encryptor.h
+++ src/crypto/encryptor.h
@@ -1,9 +1,15 @@
 #ifndef ENCRYPTOR_H
 #define ENCRYPTOR_H
-#include <string>
 #include <sodium.h>
+#include <string>
+// #include <iterator>
+#include <boost/beast/core/detail/base64.hpp>
+#include <sodium/crypto_aead_chacha20poly1305.h>
 
-void encrypt(std::string str);
+namespace base64 = boost::beast::detail::base64;
+
+std::string encrypt(std::string str);
+std::string encode(const std::string &data);
 
 #endif // !ENCRYPTOR_H
 #define ENCRYPTOR_H
